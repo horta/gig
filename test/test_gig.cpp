a3b87f934af79c9ab33ad65bc7cd9bfcc6d5c5aa
Danilo Horta <danilo.horta@gmail.com>
2016-12-29 17:02:17 +0000

    first test

--- test/test_gig.cpp
+++ test/test_gig.cpp
@@ -1,7 +1,21 @@
 #include "gig/gig.h"
+#include <iostream>
+#include <iomanip>
+#include <cassert>
+
+using std::abs;
+
+bool isclose(double x, double y) {
+  double rtol = 1e-7;
+  double atol = 0;
+  return abs(x - y) <= atol + rtol * abs(y);
+}
 
 int main(int argc, char const *argv[]) {
 
-  
+  Random random(0);
+
+  assert(isclose(random.gig(2.1, 0.1, 1.0), 1.30869321355819901));
+
   return 0;
 }
